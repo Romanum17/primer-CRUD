ceccaebb7f4ad008b0d5765306c9211de7d9cc4f
Frank <frasup17@gmail.com>
2025-05-27 11:51:22 -0500

    congiguracion basica express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,41 @@
-console.log("Hola mundo");
+import express from "express"
+import { engine } from "express-handlebars";
+import morgan from "morgan";
+import {join, dirname} from "path"
+import { fileURLToPath } from "url";
+
+//Initialitations
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+//Settings
+app.set("port", process.env.PORT || 3000);
+app.set("views", join(__dirname, "views") );
+app.engine(".hbs", engine(
+    {
+        defaultLayout: "main",
+        layoutsDir: join(app.get("views"), "layouts"),
+        partialsDir: join(app.get("views"), "partials"),
+        extname: ".hbs"
+    }
+));
+app.set("view engine", ".hbs");
+
+//Middlewares
+app.use(morgan("dev"));
+app.use(express.urlencoded ({extended:false}));
+app.use(express.json());
+
+//Routes
+app.get("/", (req, res)=>{
+    res.json({"message": "hola"})
+})
+
+//Public files
+ app.use(express.static(join( __dirname, "public")));
+
+//Run server
+app.listen(app.get("port"), ()=>
+    console.log("server listening on port", app.get("port"))
+);
+
