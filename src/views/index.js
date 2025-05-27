955878d27ca2770832188902253eef22a369ae64
Frank <frasup17@gmail.com>
2025-05-27 14:46:56 -0500

    error de nodemon

--- src/views/index.js
+++ src/views/index.js
@@ -1,16 +1,16 @@
-import express from "express"
-import { engine } from "express-handlebars";
+import express from "express";
+import {engine} from "express-handlebars";
 import morgan from "morgan";
-import {join, dirname} from "path"
-import { fileURLToPath } from "url";
+import {join, dirname} from "path";
+import {fileURLToPath} from "url";
 
-//Initialitations
+//Initializations
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 //Settings
 app.set("port", process.env.PORT || 3000);
-app.set("views", join(__dirname, "views") );
+app.set("views", join(__dirname, "views"));
 app.engine(".hbs", engine(
     {
         defaultLayout: "main",
@@ -28,14 +28,15 @@ app.use(express.json());
 
 //Routes
 app.get("/", (req, res)=>{
-    res.json({"message": "hola"})
+    res.render("index")
 })
 
 //Public files
- app.use(express.static(join( __dirname, "public")));
+ app.use(express.static(join(__dirname, "public")));
 
 //Run server
 app.listen(app.get("port"), ()=>
     console.log("server listening on port", app.get("port"))
 );
+
 
